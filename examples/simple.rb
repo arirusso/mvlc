9ec8dc7b6c96cc41b13a0c1c21a36fa0510cee2a
Ari Russo <ari.russo@gmail.com>
2017-06-19 11:43:57 -0400

    working example

--- examples/simple.rb
+++ examples/simple.rb
@@ -1,35 +1,34 @@
 #!/usr/bin/env ruby
 $:.unshift(File.join("..", "lib"))
 
-# Assign MIDI controls to MPlayer
-
+# Assign MIDI controls to VLC
 require "mvlc"
 
 # Prompt the user to select a MIDI input
 @input = UniMIDI::Input.gets
 
-@player = MVLC.new(@input, :mplayer_flags => "-fs") do
+@player = MVLC.new(@input) do
 
   # Subscribe to MIDI channel 0 only for received messages.
   # MVLC will respond to all channels by default
   rx_channel 0
 
   # When a MIDI start message is received, start playing the media file 1.mov
-  system(:start) { play("1.mov") }
+  note(1) { play("../test/media/1.mov") }
 
   # When MIDI note 1 is received: a media file 2.mov will begin playing
-  note(1) { play("2.mov") }
+  note(2) { play("../test/media/2.mov") }
 
   # When MIDI note C2 (aka 36) is received: a media file 3.mov will begin playing
-  note("C2") { play("3.mov") }
+  note(3) { play("../test/media/3.mov") }
 
   # When MIDI control change 1 is received, the media volume is set to the received value
-  cc(1) { |value| volume(:set, value) }
+  cc(1) { |value| volume(value) }
 
   # When MIDI control change 20 is received...
   cc(20) do |value|
     percent = to_percent(value) # The received value is converted to percentage eg 0..100
-    seek(percent, :percent) # That position in the media file is then moved to
+    seek_percent(percent) # That position in the media file is then moved to
   end
 
   # In addition to MIDI callbacks, there are callbacks for the player:
@@ -41,8 +40,5 @@ end
 
 # Start listening for the MIDI messages described above
 @player.start
-
-# MPlayer starts when `play` is called for the first time.
-# It runs in `fixed-vo` and `idle` mode and stays active until the program exits or `@player.stop` is explicitly called
 
 # The program can also be run in a background thread by passing `@player.start(:background => true)`
