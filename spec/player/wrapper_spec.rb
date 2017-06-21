9173f754ad0b6154c1b40ff4d423a54d6ef3900d
Ari Russo <ari.russo@gmail.com>
2017-06-21 13:16:40 -0400

    test for all player wrapper public methods

--- spec/player/wrapper_spec.rb
+++ spec/player/wrapper_spec.rb
@@ -9,6 +9,100 @@ describe MVLC::Player::Wrapper do
     @player = MVLC::Player::Wrapper.new
   end
 
+  context "#volume=" do
+
+    before(:each) do
+      expect(@player.player).to(receive(:volume).once.with(96.0).and_return(true))
+      @player.volume = 40
+    end
+
+    it "sets local state" do
+      expect(@player.state.volume).to(eq(96.0))
+    end
+
+  end
+
+  context "#seek_percent" do
+
+    before(:each) do
+      expect(@player.player).to(receive(:length).once.and_return(10))
+      expect(@player.player).to(receive(:seek).once.with(3.0).and_return(true))
+    end
+
+    it "seeks" do
+      expect(@player.seek_percent(30)).to(be(true))
+    end
+
+  end
+
+  context "#active?" do
+
+    context "when player resource exists" do
+
+      it "returns true" do
+        expect(@player.active?).to(be(true))
+      end
+
+    end
+
+    context "when player resource doesn't exist" do
+
+      before(:each) do
+        @player.instance_variable_set("@player", nil)
+      end
+
+      it "returns false" do
+        expect(@player.active?).to(be(false))
+      end
+
+    end
+
+  end
+
+  context "#pause" do
+
+    before(:each) do
+      expect(@player.state.paused?).to(be(false))
+      expect(@player.player).to(receive(:pause).once.and_return(true))
+      @player.pause
+    end
+
+    it "sets local state" do
+      expect(@player.state.paused?).to(be(true))
+    end
+
+  end
+
+  context "#add_progress_callback" do
+
+    before(:each) do
+      @callback = Proc.new { |progress| puts "hello" }
+      @player.add_progress_callback(&@callback)
+      @callbacks = @player.instance_variable_get("@callback")
+    end
+
+    it "sets progress callback" do
+      expect(@callbacks[:progress]).to_not(be_nil)
+      expect(@callbacks[:progress]).to(eq(@callback))
+    end
+
+  end
+
+  context "#add_end_of_file_callback" do
+
+    before(:each) do
+      @callback = Proc.new { puts "hello" }
+      @player.add_end_of_file_callback(&@callback)
+      @callbacks = @player.instance_variable_get("@callback")
+    end
+
+    it "sets progress callback" do
+      expect(@callbacks[:end_of_file]).to_not(be_nil)
+      expect(@callbacks[:end_of_file]).to(eq(@callback))
+    end
+
+  end
+
   context "#quit" do
 
     before(:each) do
